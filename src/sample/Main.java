befff4018befd03681268fffaad5ffcdc60207d3
Ambuj <ambuj20358@iiitd.ac.in>
2021-12-16 20:57:44 +0530

    Structured the repository

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -14,8 +15,10 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(root, 600, 400);
         String css = "style.css";
+        Image logo = new Image("logo.png");
 
-        primaryStage.setTitle("Snakes & Ladders");
+        primaryStage.getIcons().add(logo);
+        primaryStage.setTitle("Snake & Ladder Game");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
